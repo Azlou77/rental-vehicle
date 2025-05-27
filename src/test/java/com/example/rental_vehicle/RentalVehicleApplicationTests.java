b8f52f02dbae4ca1672469ec15406ba55244e644
Nguyen Louis <nguyenlouis172@gmail.com>
2025-05-27 19:33:14 +0200

    setup files

---
+++ src/test/java/com/example/rental_vehicle/RentalVehicleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rental_vehicle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalVehicleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
