c8e25e62bab94fbff5d0ca2d988ca9fb2dd7e133
Nguyen Louis <nguyenlouis172@gmail.com>
2025-06-05 18:29:37 +0200

    add Repository & Service Vehicle

---
+++ src/main/java/com/example/catalogue/repository/VehicleRepository.java
@@ -0,0 +1,12 @@
+package com.example.catalogue.repository;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.catalogue.entity.Vehicle;
+
+public interface VehicleRepository extends JpaRepository<Vehicle, Long>{
+	List<Vehicle> findByVehicleType(String vehicle_type);
+	
+	
+
+}
---
+++ src/main/java/com/example/catalogue/service/VehicleService.java
@@ -0,0 +1,26 @@
+package com.example.catalogue.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.catalogue.entity.Vehicle;
+import com.example.catalogue.repository.VehicleRepository;
+
+@Service
+public class VehicleService {
+
+	    @Autowired
+	    private VehicleRepository vehicleRepository;
+
+	    public List<Vehicle> getAllVehicles() {
+	        return vehicleRepository.findAll();
+	    }
+
+	    public List<Vehicle> getVehiclesByType(String vehicle_type) {
+	        return vehicleRepository.findByVehicleType(vehicle_type);
+	    }
+}
+
+
